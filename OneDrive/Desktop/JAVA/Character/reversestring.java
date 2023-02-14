5d02a3d9f9d94f335eedcddead26b246b20df15a
monikamr2329 <monikamr2329@gmail.com>
2023-02-14 19:02:40 +0530

    msg

---
+++ OneDrive/Desktop/JAVA/Character/reversestring.java
@@ -0,0 +1,14 @@
+class  reversestring
+{
+	public static void main(String[] args) 
+	{
+		String str="java";
+		String reverse=" ";
+		for(int i=3;i>=0;i--);
+		{
+			reverse=reverse+str.charAt();
+			System.out.println(reverse);
+		}
+		System.out.println(reverse);
+	}
+}
